80c3d65b9ec6d92bac6b0acb453a8a9b0b01ad47
samehsellami <samehsellami@outlook.com>
2025-11-10 10:30:33 +0100

    branche 8

--- src/tn/esprit/gestionzoo/entities/Aquatic.java
+++ src/tn/esprit/gestionzoo/entities/Aquatic.java
@@ -1,14 +1,28 @@
 package tn.esprit.gestionzoo.entities;
 
-public class Aquatic extends Animal{
+import tn.esprit.gestionzoo.entities.Food;
+
+
+public class Aquatic extends Animal implements Carnivore<Food> {
     String habitat;
     public Aquatic() {}
-    public Aquatic ( String family, String name,int age,boolean isMammal,String habitat ){
+
+    public Aquatic(String family, String name, int age, boolean isMammal, String habitat) {
         super(family, name, age, isMammal);
-        this.habitat= habitat;
-    }
+        this.habitat = habitat;}
+
     public String getHabitat() {
-        return habitat; }
+        return habitat;}
     public void setHabitat(String habitat) {
-              this.habitat = habitat; }
+        this.habitat = habitat;
+    }
+    @Override
+    public void eatMeat(Food meat) {
+        if (meat == Food.MEAT || meat == Food.BOTH) {
+            System.out.println(getName() + " yekel f meat");
+        } else {
+            System.out.println(getName() + " yekelch food");
+        }
+    }
 }
+
---
+++ src/tn/esprit/gestionzoo/entities/Food.java
@@ -0,0 +1,8 @@
+package tn.esprit.gestionzoo.entities;
+
+public enum Food {
+    MEAT,
+    PLANT,
+    BOTH
+}
+
